09e598837b6653fbacb28a0908158df2ee961392
felofsson <elofsson.fredrik@gmail.com>
2017-12-18 12:15:20 +0100

    from and size, for pagination

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-
-
-
-
-
-def make_es_query(search_str):
+def make_es_query(search_str, from_, size):
     """Returns ElasticSearch query for search_str,
         Matches fields
             'title'
@@ -29,17 +24,20 @@ def make_es_query(search_str):
         query_dict['query']['bool']['should'].append({'match': {field: {'query': search_str,
                                                                         'operator': 'and'}}})
 
+    query_dict['size'] = size
+    query_dict['from'] = from_
+
     # print(query_dict)
     return query_dict
 
 
-def es_search(search_str):
+def es_search(search_str, from_=0, size=100):
 
     from elasticsearch import Elasticsearch
 
     es = Elasticsearch(['localhost:9200/'])
 
-    query_dict = make_es_query(search_str)
+    query_dict = make_es_query(search_str, from_, size)
 
     res = es.search(index="documents", body=query_dict)
 
@@ -50,6 +48,8 @@ if __name__ == "__main__":
 
 
     d = es_search("volvo xc90")
+
+    print(d['hits']['total'])
 
     for hit in d['hits']['hits']:
         print(hit)
--- restapi.py
+++ restapi.py
@@ -2,7 +2,9 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 # from sqlalchemy import create_engine
 # from json import dumps
-# from flask.ext.jsonpify import jsonpify
+
+from flask import jsonify
+import main
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,14 +13,28 @@ api = Api(app)
 class Search(Resource):
     def get(self):
         search_str = request.args.get('str')
+        from_ = request.args.get('from')
+        size = request.args.get('size')
 
-        print(search_str)
+        # Parse from
+        if from_ is None or int(from_) < 0:
+            from_ = 0
 
-        return search_str
+        # Parse size
+        if size is None or int(size) < 1:
+            size = 100
+
+        res = main.es_search(search_str, from_=from_, size=size)
+
+        for hit in res['hits']['hits']:
+            print(hit)
+
+        return jsonify(res)
 
 
 api.add_resource(Search, '/search')
 
 
 if __name__ == '__main__':
-    app.run(port=5002)
+    app.run(port=5002, debug=True)
+    # Runs on http://127.0.0.1:5002/search?str=teststr
